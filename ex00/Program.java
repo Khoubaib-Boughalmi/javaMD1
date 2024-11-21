5f617a2d45ebbf6896929418c1984a8f3f86b479
Khoubaib Boughalmi <khoubaib.kb99@gmail.com>
2024-11-21 18:57:23 +0100

    ex00

---
+++ ex00/Program.java
@@ -0,0 +1,20 @@
+public class Program {
+    public static int numberCalculator(int nb) {
+        int sum = 0;
+        int count = 0
+        while (nb) {
+            sum += nb % 10;
+            nb /= 10
+            count += 1
+        }
+        if (count != 5) return -1;
+        return sum;
+    }
+
+    public static void main (String[] args) {
+        int nb = 123;
+        sum = numberCalculator(nb);
+        if (sum == -1) return 0;
+        system.out.println(sum);
+    }
+}
