66f5886419f0357abd21d8bcb7093a0eecb29bcd
khoubaib boughalmi <khoubaib.kb99@gmail.com>
2024-11-22 00:39:42 +0100

    fixed ex02

--- ex02/Program.java
+++ ex02/Program.java
@@ -1,3 +1,38 @@
+
+import java.util.Scanner;
+
 public class Program {
-    public static void main(String[] args) {}
+
+    public static int sumDigits(int nb) {
+        int sum = 0;
+        while (nb > 0) {
+            sum += nb % 10;
+            nb /= 10;
+        }
+        return sum;
+    }
+
+    public static boolean isPrime(int nb) {
+        int i;
+        for (i = 2; i * i <= nb; ++i) {
+            if (nb % i == 0) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public static void main(String[] args) {
+        int count_primes = 0;
+        try(Scanner scanner = new Scanner(System.in)){
+            while (true) {
+                int nb = scanner.nextInt();
+                if (nb == 42) {
+                    System.out.printf("Count of coffee-request: %d\n",  count_primes);
+                    break;
+                }
+                if (isPrime(sumDigits(nb))) count_primes += 1;
+            }
+        }
+    }
 }
