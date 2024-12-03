6bf408601807ca1795cb11984f3f67b19c6fa5ad
Khoubaib Boughalmi <khoubaib.kb99@gmail.com>
2024-12-03 18:09:40 +0100

    finished ex03

--- ex03/Program.java
+++ ex03/Program.java
@@ -1,12 +1,25 @@
 import java.util.Scanner;
 
 public class Program {
+    
+    public static void printArrow (int weekCount, long scoreCount) {
+        System.out.printf("Week %d ", weekCount);
+        for (int i = 0; i < scoreCount; ++i) {
+            System.out.print("=");
+        }
+        System.out.println(">");
+    }
     public static void main(String[] args) {
+        int i = 1;
+        long result = 0;
+        boolean isEven = true;
+        int multiplier = 1;
         Scanner scanner = new Scanner(System.in);
 
-        for (int i = 0; i < 18; ++i) {
+        while (true) {
+            if (i == 19) break;
             if (!scanner.hasNextLine()) {
-                System.err.println("IllegalArgument");
+                System.err.println("IllegalArgument ");
                 System.exit(-1);
             }
 
@@ -14,42 +27,68 @@ public class Program {
             Scanner lineScanner = new Scanner(line);  // Use a new scanner to process this line
 
             if (!lineScanner.hasNext()) {  // If the line is empty, it's invalid
-                System.err.println("IllegalArgument");
+                System.err.println("IllegalArgument 1");
                 System.exit(-1);
             }
+            String lineStart = "";
+            int numberStart = 0;
+            if (lineScanner.hasNextInt()) {
+                numberStart = lineScanner.nextInt(); // Get the first score
+            } else {
+                lineStart = lineScanner.next();  // Get the first token
+            }
 
-            String input = lineScanner.next();  // Get the first token
-
-            if (input.equals("42")) {
+            if (numberStart == 42) {
                 break;  // Exit the loop if "42" is encountered
             }
 
-            if (input.equals("Week")) {
+            if (isEven == true && lineStart.equals("Week")) {
                 if (lineScanner.hasNextInt()) {  // Check if there's an integer after "Week"
                     int weekNumber = lineScanner.nextInt();  // Get the week number
 
-                    if (weekNumber != i + 1) {
-                        System.err.println("IllegalArgument");
+                    if (weekNumber != i) {
+                        System.err.println("IllegalArgument 2");
+                        System.out.println(weekNumber + " " + i);
                         System.exit(-1);
                     }
 
                     // Ensure there are no additional tokens after the week number
                     if (lineScanner.hasNext()) {
-                        System.err.println("IllegalArgument");
+                        System.err.println("IllegalArgument 3");
                         System.exit(-1);
                     }
                 } else {
-                    System.err.println("IllegalArgument");
+                    System.err.println("IllegalArgument 4");
                     System.exit(-1);
                 }
+                i += 1; //increment i only one a week
+            } else if (isEven == false){
+                int min = numberStart;
+                for (int j = 0; j < 4; j++) {
+                    if (lineScanner.hasNextInt()) {
+                        int number = lineScanner.nextInt();
+                        if (number < min) min = number;
+                    } else {
+                        System.err.println("IllegalArgument 5");
+                        System.exit(-1);    
+                    }
+                }
+                result += multiplier * min;
+                multiplier *= 10;
             } else {
-                System.err.println("IllegalArgument");
+                System.err.println("IllegalArgument 6");
                 System.exit(-1);
             }
+            isEven = !isEven;
 
-            lineScanner.close();  // Close the line-specific scanner
+            lineScanner.close();
         }
-
+        int count = 1;
+        while (result != 0) {
+            printArrow(count, result % 10);
+            result /= 10;
+            count ++;
+        }
         scanner.close();
     }
 }
